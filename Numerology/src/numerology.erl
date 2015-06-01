8d66a3d03b1640ce959103bafe765e1c76ff0d78
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2015-06-01 21:14:12 +0200

    Redesigns so that interface function is seperated from each functionality Includes second step - step_02.txt

--- Numerology/src/numerology.erl
+++ Numerology/src/numerology.erl
@@ -1,11 +1,37 @@
 -module(numerology).
 
--export([replace/1]).
+-export([replace/1,
+	 replace_nine_by_ten/1,
+	 replace_two_by_one/1,
+	 replace_six_by_three/1
+	]).
 
 replace(Numbers) ->
-    lists:foldr(fun replace_nine_by_two_tens/2,[],Numbers).
+   lists:foldl(fun(Fun,Acc) -> Fun(Acc) end,
+		Numbers,
+		[fun replace_nine_by_ten/1,
+		 fun replace_two_by_one/1,
+		 fun replace_six_by_three/1]).
 
-replace_nine_by_two_tens(9,Acc) -> 
-    [10,10 | Acc];
-replace_nine_by_two_tens(Number,Acc) -> 
-    [Number | Acc].
+replace_nine_by_ten([]) ->
+    [];
+replace_nine_by_ten([9|Numbers]) ->
+    [10,10 | replace_nine_by_ten(Numbers)];
+replace_nine_by_ten([Number|Numbers]) ->
+    [Number | replace_nine_by_ten(Numbers)].
+
+replace_two_by_one([]) ->
+    [];
+replace_two_by_one([Number,2|Numbers]) ->
+    [Number] ++ lists:duplicate(Number,1) ++ replace_two_by_one(Numbers);
+replace_two_by_one([Number|Numbers]) ->
+    [Number | replace_two_by_one(Numbers)].
+
+replace_six_by_three([]) ->
+    [];
+replace_six_by_three([Number,6|Numbers])->
+    [Number] ++ lists:duplicate(lists:nth(Number,Numbers),3) ++
+	replace_six_by_three(Numbers);
+replace_six_by_three([Number|Numbers]) ->
+    [Number | replace_six_by_three(Numbers)].
+    
--- Numerology/test/numerology_tests.erl
+++ Numerology/test/numerology_tests.erl
@@ -2,8 +2,22 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-replace_nines_by_tens_test() ->
+replacer_test() ->
     Input = [1,2,3,4,5,6,7,8,9,10],
     Res = numerology:replace(Input),
+    ?assertEqual([1,1,3,4,5,3,3,3,3,3,3,3,3,3,3,7,8,10,10,10],Res).
+
+replace_9_by_two_tens_test() ->
+    Input = [1,2,3,4,5,6,7,8,9,10],
+    Res = numerology:replace_nine_by_ten(Input),
     ?assertEqual([1,2,3,4,5,6,7,8,10,10,10],Res).
-    
+
+replace_2_by_equal_amount_of_ones_as_number_to_left_of_2_test() ->
+    Input = [3,2,3,4,5],
+    Res = numerology:replace_two_by_one(Input),
+    ?assertEqual([3,1,1,1,3,4,5], Res).
+
+replace_6_by_equal_amount_of_threes_as_described_in_the_step_02_test() ->
+    Input = [1,6,3,4,5],
+    Res = numerology:replace_six_by_three(Input),
+    ?assertEqual([1,3,3,3,3,4,5], Res).    
