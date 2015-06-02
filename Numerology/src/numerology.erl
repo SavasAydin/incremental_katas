95a81f1e82b5da4f9d52d741a88cd1680df7f0d4
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2015-06-02 21:53:54 +0200

    Includes third step partially

--- Numerology/src/numerology.erl
+++ Numerology/src/numerology.erl
@@ -3,7 +3,10 @@
 -export([replace/1,
 	 replace_nine_by_ten/1,
 	 replace_two_by_one/1,
-	 replace_six_by_three/1
+	 replace_six_by_three/1,
+	 replace_three_once_by_five/1,
+	 replace_four_once_by_three/1,
+	 replace_step_03/1
 	]).
 
 replace(Numbers) ->
@@ -34,4 +37,32 @@ replace_six_by_three([Number,6|Numbers])->
 	replace_six_by_three(Numbers);
 replace_six_by_three([Number|Numbers]) ->
     [Number | replace_six_by_three(Numbers)].
-    
+
+replace_step_03(Numbers) ->    
+    Funs = [fun replace_three_once_by_five/1,
+	    fun replace_four_once_by_three/1],
+    apply_next_if_first_succeed(Funs,Numbers).
+
+apply_next_if_first_succeed([],Numbers) ->
+    Numbers;
+apply_next_if_first_succeed([F|Funs],Numbers) ->
+    case F(Numbers) of
+	Numbers ->
+	    Numbers;
+	NewNumbers ->
+	    apply_next_if_first_succeed(Funs,NewNumbers)
+    end.
+
+replace_three_once_by_five([]) ->
+    [];
+replace_three_once_by_five([3,Number|Numbers]) when Number /= 5 ->
+    [5,Number|Numbers];
+replace_three_once_by_five([Number|Numbers]) ->
+    [Number | replace_three_once_by_five(Numbers)].
+
+replace_four_once_by_three([]) ->
+    [];
+replace_four_once_by_three([Number,4|Numbers]) when Number /= 5 ->
+    [Number,3|Numbers];
+replace_four_once_by_three([Number|Numbers]) ->
+    [Number | replace_four_once_by_three(Numbers)].
--- Numerology/test/numerology_tests.erl
+++ Numerology/test/numerology_tests.erl
@@ -21,3 +21,23 @@ replace_6_by_equal_amount_of_threes_as_described_in_the_step_02_test() ->
     Input = [1,6,3,4,5],
     Res = numerology:replace_six_by_three(Input),
     ?assertEqual([1,3,3,3,3,4,5], Res).    
+
+do_not_replace_4_by_a_three_if_3_not_replaced_by_five_test() ->
+    Input = [6,5,10,4],
+    Res = numerology:replace_step_03(Input),
+    ?assertEqual([6,5,10,4],Res).
+
+do_not_replace_3_by_five_in_row_if_4_not_replaced_by_three_in_between_test() ->
+    Input = [6,3,10,3,15],
+    Res = numerology:replace_step_03(Input),
+    ?assertEqual([6,5,10,3,15],Res).
+
+do_not_replace_4_by_three_in_row_if_3_not_replaced_by_five_in_between_test() ->
+    Input = [6,3,10,4,6,4],
+    Res = numerology:replace_step_03(Input),
+    ?assertEqual([6,5,10,3,6,4],Res).
+
+replace_4_by_a_three_if_3_is_replaced_by_five_test() ->
+    Input = [6,3,10,4],
+    Res = numerology:replace_step_03(Input),
+    ?assertEqual([6,5,10,3],Res).
