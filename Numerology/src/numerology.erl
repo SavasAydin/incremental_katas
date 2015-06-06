c821f80dacaa98ce9e776c766ce4154e016850e8
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2015-06-06 23:04:04 +0200

    Includes third step - step_03.txt Renames interface functions

--- Numerology/src/numerology.erl
+++ Numerology/src/numerology.erl
@@ -1,18 +1,27 @@
 -module(numerology).
 
 -export([replace/1,
-	 replace_nine_by_ten/1,
-	 replace_two_by_one/1,
-	 replace_six_by_three/1,
-	 replace_step_03/1
+	 step_01/1,
+	 step_02/1,
+	 step_03/1
 	]).
 
 replace(Numbers) ->
    lists:foldl(fun(Fun,Acc) -> Fun(Acc) end,
 		Numbers,
-		[fun replace_nine_by_ten/1,
-		 fun replace_two_by_one/1,
-		 fun replace_six_by_three/1]).
+	       [fun step_01/1,
+		fun step_02/1
+		%%fun step_03/1
+	       ]).
+
+step_01(Numbers) ->
+    replace_nine_by_ten(Numbers).
+
+step_02(Numbers) ->
+    replace_six_by_three(replace_two_by_one(Numbers)).
+
+step_03(Numbers) ->
+    replace_three_by_five_and_four_by_three(Numbers).
 
 replace_nine_by_ten([]) ->
     [];
@@ -36,23 +45,72 @@ replace_six_by_three([Number,6|Numbers])->
 replace_six_by_three([Number|Numbers]) ->
     [Number | replace_six_by_three(Numbers)].
 
-replace_step_03(Numbers) ->    
-    replace_three_by_five_and_four_by_three(Numbers).
 
-replace_three_by_five_and_four_by_three([]) ->
+replace_three_by_five_and_four_by_three(Numbers) ->
+    run_checks([fun replace_three_by_five/1,
+		fun replace_four_by_three/1],
+	       Numbers,
+	       [],
+	       0).
+
+run_checks(_,Remaining,Result,7) ->
+    Result++Remaining;
+run_checks([F|Funs],Numbers,Result,Times) ->
+    case F(Numbers) of
+	Numbers ->
+	    Result ++ Numbers;
+	NewNumbers ->
+	    {Replaced,Rest} = extract_rest(NewNumbers),
+	    NewResult = Result ++ Replaced,
+	    run_checks(lists:append(Funs,[F]),Rest,NewResult,Times+1)
+    end.
+
+extract_rest(Numbers) ->
+    case lists:partition(fun(X) -> is_integer(X) end,
+			 Numbers) of 
+	{A,[]} ->
+	    {A,[]};
+	{A,[B]} ->
+	    {A,B}
+    end.
+
+replace_three_by_five([]) ->
     [];
-replace_three_by_five_and_four_by_three([3]) ->
+replace_three_by_five([3]) ->
     [5];
-replace_three_by_five_and_four_by_three([3|Numbers]) when hd(Numbers) /= 5 ->
-    [5 | replace_first_four_by_three(Numbers)];
-replace_three_by_five_and_four_by_three([Number|Numbers]) ->
-    [Number | replace_three_by_five_and_four_by_three(Numbers)].
+replace_three_by_five([3|Numbers]) when hd(Numbers) /= 5 ->
+    [5,Numbers];
+replace_three_by_five([Number|Numbers]) ->
+    [Number|replace_three_by_five(Numbers)].
 
-replace_first_four_by_three([]) ->
+replace_four_by_three([]) ->
     [];
-replace_first_four_by_three([4|Numbers]) ->
-    [3|replace_three_by_five_and_four_by_three(Numbers)];
-replace_first_four_by_three([Number,4|Numbers]) when Number /= 5 ->
-    [Number,3 | replace_three_by_five_and_four_by_three(Numbers)];
-replace_first_four_by_three([Number|Numbers]) ->
-    [Number | replace_first_four_by_three(Numbers)].
+replace_four_by_three([Number,4|Numbers]) when Number /= 5 ->
+    [Number,3,Numbers];
+replace_four_by_three([Number|Numbers]) ->
+    [Number|replace_four_by_three(Numbers)].
+
+%% replace_three_by_five_and_four_by_three([]) ->
+%%     [];
+%% replace_three_by_five_and_four_by_three([3|Numbers]) when hd(Numbers) /= 5 ->
+%%     [5 ];
+%% replace_three_by_five_and_four_by_three([Number|Numbers]) ->
+%%     [Number | replace_three_by_five_and_four_by_three(Numbers)].
+
+%% replace_three_by_five_and_four_by_three([]) ->
+%%     [];
+%% replace_three_by_five_and_four_by_three([3]) ->
+%%     [5];
+%% replace_three_by_five_and_four_by_three([3|Numbers]) when hd(Numbers) /= 5 ->
+%%     [5 | replace_first_four_by_three(Numbers)];
+%% replace_three_by_five_and_four_by_three([Number|Numbers]) ->
+%%     [Number | replace_three_by_five_and_four_by_three(Numbers)].
+
+%% replace_first_four_by_three([]) ->
+%%     [];
+%% replace_first_four_by_three([4|Numbers]) ->
+%%     [3|replace_three_by_five_and_four_by_three(Numbers)];
+%% replace_first_four_by_three([Number,4|Numbers]) when Number /= 5 ->
+%%     [Number,3 | replace_three_by_five_and_four_by_three(Numbers)];
+%% replace_first_four_by_three([Number|Numbers]) ->
+%%     [Number | replace_first_four_by_three(Numbers)].
--- Numerology/test/numerology_tests.erl
+++ Numerology/test/numerology_tests.erl
@@ -2,62 +2,72 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-replacer_test() ->
+perform_all_steps_test() ->
     Input = [1,2,3,4,5,6,7,8,9,10],
     Res = numerology:replace(Input),
     ?assertEqual([1,1,3,4,5,3,3,3,3,3,3,3,3,3,3,7,8,10,10,10],Res).
 
-step_00_replace_9_by_two_tens_test() ->
+step_01_replace_9_by_two_tens_test() ->
     Input = [1,2,3,4,5,6,7,8,9,10],
-    Res = numerology:replace_nine_by_ten(Input),
+    Res = numerology:step_01(Input),
     ?assertEqual([1,2,3,4,5,6,7,8,10,10,10],Res).
 
-step_01_replace_2_by_ones_test() ->
+step_02_replace_2_by_ones_test() ->
     Input = [3,2,3,4,5],
-    Res = numerology:replace_two_by_one(Input),
+    Res = numerology:step_02(Input),
     ?assertEqual([3,1,1,1,3,4,5], Res).
 
 step_02_replace_6_by_threes_test() ->
     Input = [1,6,3,4,5],
-    Res = numerology:replace_six_by_three(Input),
+    Res = numerology:step_02(Input),
     ?assertEqual([1,3,3,3,3,4,5], Res).    
 
 step_03_do_not_replace_4_by_a_three_test() ->
     Input = [6,5,10,4],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,4],Res).
 
 step_03_do_not_replace_second_3_by_five_test() ->
     Input = [6,3,10,3,15],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3,15],Res).
 
 step_03_do_not_replace_second_4_by_three_test() ->
     Input = [6,3,10,4,6,4],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3,6,4],Res).
 
 step_03_replace_both_3_by_five_and_4_by_a_three_test() ->
     Input = [6,3,10,4],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3],Res).
 
 step_03_replace_second_3_by_five_test() ->
     Input = [6,3,10,4,3],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3,5],Res).    
 
 step_03_replace_second_4_by_three_test() ->
     Input = [6,3,10,4,3,7,4],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3,5,7,3],Res).    
 
 step_03_replace_third_3_by_five_test() ->
     Input = [6,3,10,4,3,7,4,9,3,10],
-    Res = numerology:replace_step_03(Input),
+    Res = numerology:step_03(Input),
     ?assertEqual([6,5,10,3,5,7,3,9,5,10],Res).    
 
 step_03_replace_third_4_by_three_test() ->
-    Input = [3,10,4,3,7,4,3,4,11],
-    Res = numerology:replace_step_03(Input),
-    ?assertEqual([5,10,3,5,7,3,5,3,11],Res).    
+    Input = [3,10,4,3,7,4,3,7,4,11],
+    Res = numerology:step_03(Input),
+    ?assertEqual([5,10,3,5,7,3,5,7,3,11],Res).
+
+step_03_replace_forth_3_by_five_test() ->
+    Input = [6,3,10,4,3,7,4,9,3,10,4,12,3,13],
+    Res = numerology:step_03(Input),
+    ?assertEqual([6,5,10,3,5,7,3,9,5,10,3,12,5,13],Res).    
+
+step_03_do_not_replace_forth_4_by_three_test() ->
+    Input = [6,3,10,4,3,7,4,9,3,10,4,12,3,13,4,111],
+    Res = numerology:step_03(Input),
+    ?assertEqual([6,5,10,3,5,7,3,9,5,10,3,12,5,13,4,111],Res).
