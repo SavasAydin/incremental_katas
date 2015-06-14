ef30170aca0eb5e7f3af5ea0a20e372a4ef67248
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2015-06-14 23:29:00 +0200

    Includes most moves and refactors previous solution

--- Chaos_driven_development/src/chaos_driven_development.erl
+++ Chaos_driven_development/src/chaos_driven_development.erl
@@ -1,19 +1,21 @@
 -module(chaos_driven_development).
 
--export([visualize/0,
-	 create_matrix/0,
-	 init_place_of_robot/1]).
+-export([visualize_game_state/0]).
 
 -define(COMMANDS_FILE, "/tmp/commands.txt").
 -define(STATES_FILE, "/tmp/states.txt").
 
-visualize() ->
+visualize_game_state() ->
+    M = initialize_robot_in_matrix(),
+    Commands = read_commands(),
+    perform_commands(Commands,M).
+
+initialize_robot_in_matrix() ->
     M = create_matrix(),
     NewM = init_place_of_robot(M),    
     Res = visualize_state(NewM),
-    ok = write_states(list_to_binary(Res)),
-    Commands = read_commands(),
-    perform_commands(Commands,NewM).
+    ok = write_states(Res),
+    NewM.
 
 create_matrix() ->
     [ {{X,Y},Z} || X <- [1,2], Y <- [1,2], Z <- [" "] ].
@@ -25,12 +27,10 @@ read_commands() ->
     {ok, Bin} = file:read_file(?COMMANDS_FILE),
     string:tokens(binary_to_list(Bin),"\n").
 
-perform_commands([],State) ->
-    State;
 perform_commands(Commands,State) ->
     lists:foldl(fun(Command,S) -> NewState = perform_command(Command,S),
 				  Res = visualize_state(NewState),
-				  write_states(list_to_binary(Res)),
+				  write_states(Res),
 				  NewState
 		end,
 		State,
@@ -49,18 +49,55 @@ generate_newline() ->
 perform_command([],State) ->
     State;
 perform_command("DF",State) ->
-    {{X,Y}, Direction} = get_robot_position(State),
-    S2 = lists:keyreplace({X,Y}, 1, State, {{X,Y}, " "}),
-    lists:keyreplace({X-1,Y}, 1, S2, {{X-1,Y}, Direction});
+    Position = get_robot_position(State),
+    move_forward(Position,State);
+perform_command("DB",State) ->
+    Position = get_robot_position(State),
+    move_backward(Position,State);
 perform_command("TR",State) ->
-    {{X,Y}, Direction} = get_robot_position(State),
-    lists:keyreplace({X,Y}, 1, State, {{X,Y}, "E"}).            
+    Position = get_robot_position(State),
+    turn_right(Position,State);   
+perform_command("TL",State) ->
+    Position = get_robot_position(State),
+    turn_left(Position,State).
 
-    
-get_robot_position([{_," "}|Rest]) ->
+get_robot_position([{_," "}|Rest]=State) ->
     get_robot_position(Rest);
 get_robot_position([H|_]) ->
     H.
 
-write_states(Actions) ->    
-    file:write_file(?STATES_FILE, Actions, [append]).
+move_forward({Key={X,Y},Direction},State) ->
+    S = update_position(Key," ",State), 
+    NewPos = proplists:get_value(Direction, [{"N",{X-1,Y}},
+					     {"W",{X,Y-1}},
+					     {"E",{X,Y+1}},
+					     {"S",{X+1,Y}}
+					    ]),
+    update_position(NewPos,Direction,S).
+
+move_backward({Key={X,Y},Direction},State) ->
+    S = update_position(Key," ",State),
+    NewPos = proplists:get_value(Direction, [{"N",{X+1,Y}},
+					     {"S",{X-1,Y}},
+					     {"E",{X,Y-1}},
+					     {"W",{X,Y+1}}]),
+    update_position(NewPos,Direction,S).
+    
+turn_right({Key,Direction},State) ->
+    NewDir = proplists:get_value(Direction, [{"N","E"},
+					     {"S","W"},
+					     {"W","N"},
+					     {"E","S"}]),
+    update_position(Key,NewDir,State).
+
+turn_left({Key,Direction},State) ->
+    NewDirection = proplists:get_value(Direction, [{"N","W"},
+					     {"E","N"}
+					    ]),
+    update_position(Key,NewDirection,State).
+
+update_position(Key,Value,State) ->
+    lists:keyreplace(Key, 1, State, {Key, Value}).
+
+write_states(Result) ->    
+    file:write_file(?STATES_FILE, list_to_binary(Result), [append]).
--- Chaos_driven_development/test/chaos_driven_development_tests.erl
+++ Chaos_driven_development/test/chaos_driven_development_tests.erl
@@ -6,31 +6,111 @@ state_test_() ->
     {foreach,
      fun setup/0,
      fun cleanup/1,
-     [fun initial_state_if_no_command/0,
+     [
+      fun initial_state_if_no_command/0,
       fun drive_forward/0,
-      fun drive_forward_and_turn_right/0
+      fun drive_forward_and_drive_backward/0,
+      fun drive_forward_and_turn_right/0,
+      fun turn_right_and_turn_left/0,
+      fun turn_right_three_times/0,
+      fun drive_forward_turn_right_and_drive_backward/0,
+      fun turn_right_twice_and_drive_backward/0,
+      fun turn_left_and_drive_forward/0,
+      fun turn_left_drive_forward_and_drive_backward/0,
+      fun turn_left_drive_forward_and_turn_right_drive_forward_three_times/0
      ]
     }.
 
 initial_state_if_no_command() ->
-    chaos_driven_development:visualize(),
+    chaos_driven_development:visualize_game_state(),
     {ok, Bin} = file:read_file("/tmp/states.txt"),
     ?assertEqual(<<"[ ][ ]\n[ ][N]\n">>, Bin).       
 
 drive_forward() ->
     CommandsFile = "/tmp/commands.txt",
     ok = file:write_file(CommandsFile, <<"DF">>),
-    chaos_driven_development:visualize(),
+    chaos_driven_development:visualize_game_state(),
     {ok, Bin} = file:read_file("/tmp/states.txt"),
     ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][N]\n[ ][ ]\n">>, Bin).  
+
+drive_forward_and_drive_backward() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"DF\nDB">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n"
+		   "[ ][N]\n[ ][ ]\n"
+		   "[ ][ ]\n[ ][N]\n">>, Bin).  
 
 drive_forward_and_turn_right() ->
     CommandsFile = "/tmp/commands.txt",
     ok = file:write_file(CommandsFile, <<"DF\nTR">>),
-    chaos_driven_development:visualize(),
+    chaos_driven_development:visualize_game_state(),
     {ok, Bin} = file:read_file("/tmp/states.txt"),
     ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][N]\n[ ][ ]\n[ ][E]\n[ ][ ]\n">>, Bin).  
 
+turn_right_and_turn_left() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TR\n\TL">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n"
+		   "[ ][ ]\n[ ][E]\n"
+		   "[ ][ ]\n[ ][N]\n">>, Bin).  
+
+drive_forward_turn_right_and_drive_backward() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"DF\nTR\nDB">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][N]\n[ ][ ]\n[ ][E]\n[ ][ ]\n[E][ ]\n[ ][ ]\n">>, Bin).  
+
+turn_right_twice_and_drive_backward() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TR\n\TR\nDB">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n"
+		   "[ ][ ]\n[ ][E]\n"
+		   "[ ][ ]\n[ ][S]\n"
+		   "[ ][S]\n[ ][ ]\n">>, Bin).  
+
+turn_right_three_times() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TR\n\TR\nTR">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n"
+		   "[ ][ ]\n[ ][E]\n"
+		   "[ ][ ]\n[ ][S]\n"
+		   "[ ][ ]\n[ ][W]\n">>, Bin).  
+    
+turn_left_and_drive_forward() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TL\nDF">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][ ]\n[ ][W]\n[ ][ ]\n[W][ ]\n">>, Bin).  
+
+turn_left_drive_forward_and_drive_backward() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TL\nDF\nDB">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][ ]\n[ ][W]\n"
+		   "[ ][ ]\n[W][ ]\n[ ][ ]\n[ ][W]\n">>, Bin).  
+
+turn_left_drive_forward_and_turn_right_drive_forward_three_times() ->
+    CommandsFile = "/tmp/commands.txt",
+    ok = file:write_file(CommandsFile, <<"TL\nDF\nTR\nDF\nTR\nDF\nTR\nDF">>),
+    chaos_driven_development:visualize_game_state(),
+    {ok, Bin} = file:read_file("/tmp/states.txt"),
+    ?assertEqual(<<"[ ][ ]\n[ ][N]\n[ ][ ]\n[ ][W]\n[ ][ ]\n[W][ ]\n"
+		   "[ ][ ]\n[N][ ]\n[N][ ]\n[ ][ ]\n[E][ ]\n[ ][ ]\n"
+		   "[ ][E]\n[ ][ ]\n[ ][S]\n[ ][ ]\n[ ][ ]\n[ ][S]\n">>, Bin).  
+
+
+    
 setup() ->
     CommandsFile = "/tmp/commands.txt",
     ok = file:write_file(CommandsFile, <<>>),
@@ -40,13 +120,3 @@ setup() ->
 
 cleanup(Files) ->
     [ file:delete(File) || File <- Files ].
-
-    
-create_matrix_test() ->
-    Res = chaos_driven_development:create_matrix(),
-    ?assertEqual([{{1,1}," "},{{1,2}," "},{{2,1}," "},{{2,2}," "}], Res).
-
-initialize_robot_in_matrix_test() ->
-    M = chaos_driven_development:create_matrix(),
-    Res = chaos_driven_development:init_place_of_robot(M),
-    ?assertEqual([{{1,1}," "},{{1,2}," "},{{2,1}," "},{{2,2},"N"}], Res).
