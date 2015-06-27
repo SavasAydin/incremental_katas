4eba7f6e5a2ef37f9329dd115fd533830dc0cdeb
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2015-06-27 21:26:51 +0200

    refactors and remove duplication and unneccessary funtion clauses

--- Chaos_driven_development/src/chaos_driven_development.erl
+++ Chaos_driven_development/src/chaos_driven_development.erl
@@ -8,7 +8,7 @@
 visualize_game_state(Dimension) ->
     M = initialize_robot_in_matrix(Dimension),
     Commands = read_commands(),
-    perform_commands(Commands,M).
+    perform_commands_and_store_results(Commands,M).
 
 initialize_robot_in_matrix(Dimension) ->
     M = create_matrix(Dimension),
@@ -26,68 +26,68 @@ read_commands() ->
     {ok, Bin} = file:read_file(?COMMANDS_FILE),
     string:tokens(binary_to_list(Bin),"\n").
 
-perform_commands(Commands,State) ->
-    lists:foldl(fun(Command,S) -> NewState = perform_command(Command,S),
-				  store_state(NewState),
-				  NewState
+perform_commands_and_store_results(Commands,State) ->
+    lists:foldl(fun(Command,S) -> 
+			NewState = perform_command(Command,S),
+			store_state(NewState),
+			NewState
 		end,
 		State,
 		Commands).
     
 perform_command([],State) ->
     State;
-perform_command("DF",State) ->
-    Position = get_robot_position(State),
-    move_forward(Position,State);
-perform_command("DB",State) ->
-    Position = get_robot_position(State),
-    move_backward(Position,State);
-perform_command("TR",State) ->
-    Position = get_robot_position(State),
-    turn_right(Position,State);   
-perform_command("TL",State) ->
-    Position = get_robot_position(State),
-    turn_left(Position,State).
+perform_command(Command,State) ->
+    Pos = get_position(State),
+    Fun = get_command_fun(Command),
+    NewPos = Fun(Pos),
+    NewState = clear_out_old_position(Pos,State),    
+    update_position(NewPos,NewState).
 
-get_robot_position([{_," "}|Rest]) ->
-    get_robot_position(Rest);
-get_robot_position([H|_]) ->
-    H.
+get_position(State) ->
+    [Pos] = lists:filter(fun({_,Direction}) -> Direction /= " " end, State),
+    Pos.
 
-move_forward({Key={X,Y},Direction},State) ->
-    S = update_position(Key," ",State), 
-    NewPos = proplists:get_value(Direction, [{"N",{X-1,Y}},
-					     {"W",{X,Y-1}},
-					     {"E",{X,Y+1}},
-					     {"S",{X+1,Y}}
-					    ]),
-    update_position(NewPos,Direction,S).
+get_command_fun(Command) ->
+    Funs = [{"DF",fun move_forward/1},
+	    {"DB",fun move_backward/1},
+	    {"TR",fun turn_right/1},
+	    {"TL",fun turn_left/1}],
+    proplists:get_value(Command,Funs).
 
-move_backward({Key={X,Y},Direction},State) ->
-    S = update_position(Key," ",State),
-    NewPos = proplists:get_value(Direction, [{"N",{X+1,Y}},
-					     {"S",{X-1,Y}},
-					     {"E",{X,Y-1}},
-					     {"W",{X,Y+1}}]),
-    update_position(NewPos,Direction,S).
+clear_out_old_position({Key,_},State) ->    
+    update_position({Key," "},State).
+
+move_forward({{X,Y},Direction}) ->
+    Coordinate = proplists:get_value(Direction, [{"N",{X-1,Y}},
+						 {"W",{X,Y-1}},
+						 {"E",{X,Y+1}},
+						 {"S",{X+1,Y}}]),
+    {Coordinate,Direction}.
+
+move_backward({{X,Y},Direction}) ->
+    Coordinate = proplists:get_value(Direction, [{"N",{X+1,Y}},
+						 {"S",{X-1,Y}},
+						 {"E",{X,Y-1}},
+						 {"W",{X,Y+1}}]),
+    {Coordinate,Direction}.
     
-turn_right({Key,Direction},State) ->
-    NewDir = proplists:get_value(Direction, [{"N","E"},
-					     {"S","W"},
-					     {"W","N"},
-					     {"E","S"}]),
-    update_position(Key,NewDir,State).
+turn_right({Coordinate,Direction}) ->
+    NewDirection = proplists:get_value(Direction, [{"N","E"},
+						   {"S","W"},
+						   {"W","N"},
+						   {"E","S"}]),
+    {Coordinate,NewDirection}.    
 
-turn_left({Key,Direction},State) ->
+turn_left({Coordinate,Direction}) ->
     NewDirection = proplists:get_value(Direction, [{"N","W"},
 						   {"E","N"},
 						   {"W","S"},
-						   {"S","E"}
-						  ]),
-    update_position(Key,NewDirection,State).
+						   {"S","E"}]),
+    {Coordinate,NewDirection}.    
 
-update_position(Key,Value,State) ->
-    lists:keyreplace(Key, 1, State, {Key, Value}).
+update_position({Coordinate,Direction},State) ->
+    lists:keyreplace(Coordinate, 1, State, {Coordinate, Direction}).
 
 store_state(State) ->
     Result = visualize_state(State),
